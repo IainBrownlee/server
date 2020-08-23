dc96d8150361b857c224ad25c5f0f6f00bb0c235
IainBrownlee <64917434+IainBrownlee@users.noreply.github.com>
2020-08-23 16:26:53 +0100

    testing auto deploy

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Methods','GET,PUT,POST');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
